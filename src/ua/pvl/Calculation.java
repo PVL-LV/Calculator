16f4818f98f95c71554f8b435a6dbd49648026e7
pvl <pvl-lv@mail.ru>
2017-03-24 18:23:05 +0200

    still not ready yet

--- src/ua/pvl/Calculation.java
+++ src/ua/pvl/Calculation.java
@@ -5,7 +5,7 @@ public abstract class Calculation {
     protected final Fraction fraction1;
     protected final Fraction fraction2;
     protected final String operation;
-    protected Fraction answer;
+    public Fraction answer;
 
     public Calculation(Fraction fraction1, Fraction fraction2, String operation) {
         this.fraction1 = fraction1;
@@ -13,6 +13,10 @@ public abstract class Calculation {
         this.operation = operation;
         this.answer = new Fraction();
         calculate();
+    }
+
+    public Fraction getAnswer() {
+        return answer;
     }
 
     public abstract void calculate();
--- src/ua/pvl/Parser.java
+++ src/ua/pvl/Parser.java
@@ -2,38 +2,63 @@ package ua.pvl;
 
 public class Parser {
 
-	public static void parse(String[] args) {
+	Calculation calc = null;
+	Fraction res;
+
+	public void parse(String[] args) {
 
 		String str = args[0];
 
 		recursion(str);
 	}
 
-	private static String recursion(String str) {
+	private String recursion(String str) {
 
         Fraction fraction1 = new Fraction(); // first fraction
         Fraction fraction2 = new Fraction(); // second fraction
 
         // TODO create method to avoid duplication of code
-        int firstElement = getElement(str, 0);
-		fraction1.setNumerator(firstElement);
+		int operandPosition;
+		String operation = "";
 
-		int operandPosition = findOperand(str);
-		String operand = getOperand(operandPosition, str);
+		if (res == null ) {
+			int firstElement = getElement(str, 0);
+			fraction1.setNumerator(firstElement);
 
-		if (operand.equals("|")) {
-            int secondElement = getElement(str, operandPosition + 1);
-            fraction1.setDenominator(secondElement);
-            str = str.replace(firstElement + operand + secondElement, "");
-        } else {
-		    fraction1.setDenominator(1);
-            str = str.replace(firstElement + operand, "");
-        }
+			operandPosition = findOperand(str, 0);
+			String operand = getOperand(operandPosition, str);
+
+			operation = "";
+			if (operand.equals("|")) {
+				operation = getOperand(findOperand(str, operandPosition + 1), str);
+			}
+
+
+			if (operand.equals("|")) {
+				int secondElement = getElement(str, operandPosition + 1);
+				fraction1.setDenominator(secondElement);
+				str = str.replace(firstElement + operand + secondElement + operation, "");
+			} else {
+				fraction1.setDenominator(1);
+				str = str.replace(firstElement + operand, "");
+				operation = operand;
+			}
+		} else {
+			operandPosition = findOperand(str, 0);
+			String operand = getOperand(operandPosition, str);
+			if (operand.equals("|")) {
+				operation = getOperand(findOperand(str, operandPosition + 1), str);
+				fraction1.setNumerator(res.getNumerator());
+				fraction1.setDenominator(res.getDenominator());
+			}
+		}
+
+
 
         int firstElement2 = getElement(str, 0);
-		fraction2.setNumerator(firstElement);
+		fraction2.setNumerator(firstElement2);
 
-		int operandPosition2 = findOperand(str);
+		int operandPosition2 = findOperand(str, 0);
 		String operand2 = getOperand(operandPosition, str);
 
 		if (operand2.equals("|")) {
@@ -45,18 +70,37 @@ public class Parser {
             str = str.replace(firstElement2 + operand2, "");
         }
 
+		Calculation calc = null;
+		Fraction res;
+        if (operation == "+") {
+			calc = new Addition(fraction1, fraction2);
+		} else if (operation == "-") {
+			calc = new Subtraction(fraction1, fraction2);
+		} else if (operation == "*") {
+			calc = new Multiplication(fraction1, fraction2);
+		} else if (operation == "/") {
+			calc = new Division(fraction1, fraction2);
+		}
 
-		if (findOperand(str) == -1) {
+		calc.calculate();
+		res = calc.getAnswer();
+
+
+
+
+
+		if (findOperand(str, 0) == -1) {
 			//System.out.println("Result is: " + result);
 			System.exit(0);
 		}
+
 
 		return recursion(str);
 	}
 
 
 
-
+    // rewrite this method
 	private static int getResult(int firstElement, int secondElement, String operand) {
 		int sum;
 		if (operand.equals("+")) {
@@ -70,44 +114,51 @@ public class Parser {
 	private static String getOperand(int operandPosition, String str) {
 		String op = "";
 		Character s = str.charAt(operandPosition);
-		if (s.equals("+")) {
+		if (s == '+') {
 			op = "+";
-		} else if (s.equals("-")) {
+		} else if (s == '-') {
 		    op = "-";
-		} else if (s.equals("*")) {
+		} else if (s == '*') {
 		    op = "*";
-        } else if (s.equals("/")) {
+        } else if (s == '/') {
 		    op = "/";
-        } else if (s.equals("|")) {
+        } else if (s == '|') {
 		    op = "|";
         }
 		return op;
 	}
 
 	private static int getElement(String str, int startPosition){
-		for (int i = 0; i <= str.length(); i++) {
+		for (int i = startPosition; i <= str.length(); i++) {
+
 			Character s = str.charAt(i);
-			String st = s.toString();
-			if (!st.equals("+") || !st.equals("-") || !st.equals("*") || !st.equals("/") || !st.equals("|")) {
+
+			if (s != '+' && s != '-' && s != '*' && s != '/' && s != '|') {
 				Character s2 = str.charAt(i+1);
-					if (!s2.equals("+") || !s2.equals("-") || !s2.equals("*") || !s2.equals("/") || !s2.equals("|")) {
+					if (s2 != '+' && s2 != '-' && s2 != '*' && s2 != '/' && s2 != '|') {
 						String st2 = s + s2.toString();
 						String res = st2;
 						return Integer.parseInt(res);
+					} else if (Character.isDigit(s)){
+						return Integer.parseInt(s.toString());
+					} else {
+						i++;
 					}
-			} else {
-				return Integer.parseInt(st);
+			} else if (Character.isDigit(s)) {
+				return Integer.parseInt(s.toString());
+			}else {
+				i++;
 			}
 		}
 		return -1; // not sure
 	}
 
-	public static int findOperand(String str) {
+	public static int findOperand(String str, int startPosition) {
 
-		for (int i = 0; i <= str.length(); i++) {
+		for (int i = startPosition; i <= str.length(); i++) {
 			Character s = str.charAt(i);
-			String st = s.toString();
-			if (st.equals("+") || st.equals("-")){
+			//String st = s.toString();
+			if (s == '+' || s == '-' || s == '*' || s == '/' || s == '|'){
 				return i;
 			}
 		}
